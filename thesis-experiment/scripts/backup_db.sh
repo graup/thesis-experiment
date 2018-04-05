89f4873eb9b0aba3400fae3ef2fee6de13bde54b
Paul Grau <paul@graycoding.com>
2018-04-05 13:05:41 +0900

    fix db script

--- thesis-experiment/scripts/backup_db.sh
+++ thesis-experiment/scripts/backup_db.sh
@@ -3,7 +3,7 @@ ssh ssh.pythonanywhere.com /bin/bash -l <<'EOF'
   mkdir -p ~/backup
   cd ~/backup
   echo "Backing up $MYSQL_USER@$MYSQL_HOST:$MYSQL_DB to $(date "+%Y_%m_%d__%H_%M").sql ..."
-  MYSQL_PWD=$MYSQL_PASSWORD mysqldump -h "$MYSQL_HOST" -u "$MYSQL_USER" "$MYSQL_DB" | gzip > $(date "+%Y_%m_%d__%H_%M").sql;
+  MYSQL_PWD=$MYSQL_PASSWORD mysqldump -h "$MYSQL_HOST" -u "$MYSQL_USER" "$MYSQL_DB" > $(date "+%Y_%m_%d__%H_%M").sql;
   echo "done";
 
 EOF
