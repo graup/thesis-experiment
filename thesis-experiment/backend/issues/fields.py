c199926fe8d9c8046f3e1f7022eba7bbe9264fde
Paul Grau <paul@graycoding.com>
2018-06-29 14:35:28 +0900

    adjust decay time

--- thesis-experiment/backend/issues/fields.py
+++ thesis-experiment/backend/issues/fields.py
@@ -9,7 +9,7 @@ from django.utils import timezone
 
 from .utils import calculate_heat
 
-DEFAULT_DECAY_TIME = 24*60*60*120 # 120 days
+DEFAULT_DECAY_TIME = 24*60*60*200 # 200 days
 DEFAULT_SCORE_BASE = 8
 
 
